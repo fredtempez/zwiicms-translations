1801ea763a1b2a58a16a29e6018a7e4e27727755
Fred Tempez <frederic.tempez@outlook.com>
2022-12-29 10:54:05 +0100

    Fix gr_G + Optimize check tool

--- tools/check.php
+++ tools/check.php
@@ -1,7 +1,9 @@
 <?php
-		$fr = json_decode(file_get_contents('../fr_FR.json'), true);
-		$pt = json_decode(file_get_contents('../en_EN.json'), true);
-		$intersec = array_intersect_key($pt, $fr);
-		$dif = array_diff_key($fr, $pt);
+		$langSource = 'fr_FR';
+        $langTarget = 'fr_FR';
+        $source = json_decode(file_get_contents('../' . $langSource . '.json'), true);
+		$target = json_decode(file_get_contents('../' . $langTarget . '.json'), true);
+		$intersec = array_intersect_key($target, $source);
+		$dif = array_diff_key($source, $target);
 		$merge = array_merge($dif, $intersec);
-		file_put_contents('../en_EN.json', json_encode($merge, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+		file_put_contents('../' . $langTarget . '.json', json_encode($merge, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
