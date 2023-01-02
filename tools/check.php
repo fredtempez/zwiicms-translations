5c55d90be1b4e7c066d1906032aa07daa05442bc
Fred Tempez <frederic.tempez@outlook.com>
2023-01-02 17:52:08 +0100

    Check all in one

--- tools/check.php
+++ tools/check.php
@@ -1,10 +1,14 @@
 <?php
 		$langSource = 'fr_FR';
-        $langTarget = 'fr_FR';
-        $source = json_decode(file_get_contents('../' . $langSource . '.json'), true);
-		$target = json_decode(file_get_contents('../' . $langTarget . '.json'), true);
-		$intersec = array_intersect_key($target, $source);
-		$dif = array_diff_key($source, $target);
-		$merge = array_merge($dif, $intersec);
-		file_put_contents('../' . $langTarget . '.json', json_encode($merge, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
-        echo 'Done...';
+        $langTargetArray = ['en_EN', 'es','gr_GR', 'it', 'pt_PT'];
+		foreach ($langTargetArray as $key => $langTarget) {
+			$source = json_decode(file_get_contents('../' . $langSource . '.json'), true);
+			$target = json_decode(file_get_contents('../' . $langTarget . '.json'), true);
+			$intersec = array_intersect_key($target, $source);
+			$dif = array_diff_key($source, $target);
+			$merge = array_merge($dif, $intersec);
+			file_put_contents('../' . $langTarget . '.json', json_encode($merge, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+			echo $langTarget . ' terminé.';
+		}
+
+        echo 'Tout est fini.';
