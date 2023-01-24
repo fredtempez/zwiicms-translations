215dd2cd6e7e2c25c64203861abf080381f9b3b4
Fred Tempez <ftempez@gmail.com>
2023-01-24 09:57:43 +0100

    form translations okay

--- tools/check.php
+++ tools/check.php
@@ -1,7 +1,7 @@
 <?php
 $langSource = 'fr_FR';
 $langTargetArray = ['en_EN', 'es', 'gr_GR', 'it', 'pt_PT'];
-$folderTarget = '../modules/blog/';
+$folderTarget = '../modules/form/';
 foreach ($langTargetArray as $key => $langTarget) {
 	if (file_exists($folderTarget . $langSource . '.json') &&
 		file_exists($folderTarget . $langTarget . '.json') )
--- tools/order.php
+++ tools/order.php
@@ -1,6 +1,6 @@
 <?php
 		$langTarget = 'gr_GR';
-        $folderTarget = '../modules/blog/';
+        $folderTarget = '../modules/form/';
         $source = json_decode(file_get_contents($folderTarget . $langTarget . '.json'), true);
         ksort($source, SORT_STRING);
 		file_put_contents($folderTarget . $langTarget . '.json', json_encode($source, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
