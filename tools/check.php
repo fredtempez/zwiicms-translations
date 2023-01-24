90ca8e9f022809e734ea58fdc66c3e50e1a7ced1
Fred Tempez <ftempez@gmail.com>
2023-01-24 11:50:39 +0100

    gallery gr_GR ok

--- tools/check.php
+++ tools/check.php
@@ -1,20 +1,30 @@
 <?php
 $langSource = 'fr_FR';
 $langTargetArray = ['en_EN', 'es', 'gr_GR', 'it', 'pt_PT'];
-$folderTarget = '../modules/form/';
-foreach ($langTargetArray as $key => $langTarget) {
-	if (file_exists($folderTarget . $langSource . '.json') &&
-		file_exists($folderTarget . $langTarget . '.json') )
-	{
-		$source = json_decode(file_get_contents($folderTarget . $langSource . '.json'), true);
-		$target = json_decode(file_get_contents($folderTarget . $langTarget . '.json'), true);
-		$intersec = array_intersect_key($target, $source);
-		$dif = array_diff_key($source, $target);
-		$merge = array_merge($dif, $intersec);
-		file_put_contents($folderTarget . $langTarget . '.json', json_encode($merge, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
-		echo $langTarget . ' terminé.';
+$folderTargetArray = ['../',
+					 '../modules/blog/',
+					 '../modules/news/',
+					 '../modules/form/',
+					 '../modules/gallery/',
+					 '../modules/redirection/',
+					 '../modules/search/',
+					];
+foreach($folderTargetArray as $keyFolder => $folderTarget) {
+	echo '<p><strong>' . $folderTarget . '</strong></p/>';
+	foreach ($langTargetArray as $key => $langTarget) {
+		if (file_exists($folderTarget . $langSource . '.json') &&
+			file_exists($folderTarget . $langTarget . '.json') )
+		{
+			$source = json_decode(file_get_contents($folderTarget . $langSource . '.json'), true);
+			$target = json_decode(file_get_contents($folderTarget . $langTarget . '.json'), true);
+			$intersec = array_intersect_key($target, $source);
+			$dif = array_diff_key($source, $target);
+			$merge = array_merge($dif, $intersec);
+			file_put_contents($folderTarget . $langTarget . '.json', json_encode($merge, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+			echo '<p>' . $langTarget . ' checked.' . '</p>';
+		}
 	}
-
 }
 
-echo 'Tout est fini.';
+
+echo '<br />All Done';
--- tools/order.php
+++ tools/order.php
@@ -1,7 +1,22 @@
 <?php
-		$langTarget = 'gr_GR';
-        $folderTarget = '../modules/form/';
-        $source = json_decode(file_get_contents($folderTarget . $langTarget . '.json'), true);
-        ksort($source, SORT_STRING);
-		file_put_contents($folderTarget . $langTarget . '.json', json_encode($source, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
-        echo 'Done...';
+$langTargetArray = ['fr_FR', 'en_EN', 'es', 'gr_GR', 'it', 'pt_PT'];
+$folderTargetArray = ['../',
+					 '../modules/blog/',
+					 '../modules/news/',
+					 '../modules/form/',
+					 '../modules/gallery/',
+					 '../modules/redirection/',
+					 '../modules/search/',
+					];
+foreach($folderTargetArray as $keyFolder => $folderTarget) {
+	echo '<p><strong>' . $folderTarget . '</strong></p/>';
+	foreach ($langTargetArray as $key => $langTarget) {
+        if (file_exists($folderTarget . $langTarget . '.json')) {
+            $source = json_decode(file_get_contents($folderTarget . $langTarget . '.json'), true);
+            ksort($source, SORT_STRING);
+            file_put_contents($folderTarget . $langTarget . '.json', json_encode($source, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+            echo 'Done...';
+        }
+
+    }
+}
