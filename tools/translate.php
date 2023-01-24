e6d1da44925645b65d1862e9986c665000181a6e
FredTempez <ftempez@gmail.com>
2023-01-24 16:38:28 +0100

    Modules translations en_EN it pt_PT + tool

---
+++ tools/translate.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$langSource = 'fr_FR';
+$langTarget = 'it';
+$folderTargetArray = [//'../',
+					 '../modules/blog/',
+					 '../modules/news/',
+					 '../modules/form/',
+					 '../modules/gallery/',
+					 '../modules/redirection/',
+					 '../modules/search/',
+					];
+foreach ($folderTargetArray as $keyFolder => $folderTarget) {
+    if (file_exists($folderTarget . $langSource . '.json')) {
+        $sourceData = json_decode(file_get_contents($folderTarget . $langSource . '.json'), true);
+        foreach ($sourceData as $originText => $targetText) {
+            $arrayjson = json_decode(file_get_contents('https://clients5.google.com/translate_a/t?client=dict-chrome-ex&sl=auto&tl=' . $langTarget . '&q=' . rawurlencode($originText)), true);
+            $targetData[$originText] = $arrayjson[0][0];
+        }
+        file_put_contents($folderTarget . $langTarget . '.json', json_encode($targetData, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+        echo $folderTarget . ' Done...';
+        $targetData = [];
+    }
+}
