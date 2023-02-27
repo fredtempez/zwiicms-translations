cfa6a561391498a47773eb8d631de154e102b6b7
Fred Tempez <frederic.tempez@outlook.com>
2023-02-27 15:25:08 +0100

    tr_TR modules and minify

---
+++ tools/minify.php
@@ -0,0 +1,21 @@
+<?php
+$langTargetArray = ['fr_FR', 'de', 'en_EN', 'es', 'el_GR', 'it', 'pt_PT'];
+$folderTargetArray = ['../',
+					 '../modules/blog/',
+					 '../modules/news/',
+					 '../modules/form/',
+					 '../modules/gallery/',
+					 '../modules/redirection/',
+					 '../modules/search/',
+					];
+foreach($folderTargetArray as $keyFolder => $folderTarget) {
+	echo '<p><strong>' . $folderTarget . '</strong></p/>';
+	foreach ($langTargetArray as $key => $langTarget) {
+        if (file_exists($folderTarget . $langTarget . '.json')) {
+            $source = json_decode(file_get_contents($folderTarget . $langTarget . '.json'), true);
+            file_put_contents($folderTarget . $langTarget . '.json', json_encode($source, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+            echo 'Done...';
+        }
+
+    }
+}
