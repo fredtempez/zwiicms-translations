f4bc54c4aa0a90cb44514fbfea8fb8590d21999d
fredtempez <frederic.tempez@outlook.com>
2023-08-05 17:40:42 +0200

    translate auto

--- tools/translate.php
+++ tools/translate.php
@@ -2,7 +2,15 @@
 
 
 $langSource = 'fr_FR';
-$langTarget = 'de';
+$langTargetArray = [
+    'de',
+    'en_EN',
+    'es',
+    'gr_GR',
+    'it',
+    'pt_PT',
+    'tr_TR'
+];
 $folderTargetArray = [
     '../',
     /*
@@ -14,21 +22,26 @@ $folderTargetArray = [
     '../modules/search/',
     */
 ];
-foreach ($folderTargetArray as $keyFolder => $folderTarget) {
-    if (file_exists($folderTarget . $langSource . '.json')) {
-        $sourceData = json_decode(file_get_contents($folderTarget . $langSource . '.json'), true);
-        foreach ($sourceData as $originText => $targetText) {
-            if (empty($targetText)) {
+foreach ($langTargetArray as $langTarget) {
+    echo $langTarget;
+    echo '<p>';
+    foreach ($folderTargetArray as $keyFolder => $folderTarget) {
+        if (file_exists($folderTarget . $langSource . '.json')) {
+            $sourceData = json_decode(file_get_contents($folderTarget . $langSource . '.json'), true);
+            foreach ($sourceData as $originText => $targetText) {
                 echo '<p>';
                 echo $originText;
+                echo ' - ';
+                if (empty($targetText)) {
+                    echo $targetText;
+                    $arrayjson = json_decode(file_get_contents('https://clients5.google.com/translate_a/t?client=dict-chrome-ex&sl=auto&tl=' . $langTarget . '&q=' . rawurlencode($originText)), true);
+                    $targetData[$originText] = $arrayjson[0][0];
+                }
                 echo '</p>';
-                $arrayjson = json_decode(file_get_contents('https://clients5.google.com/translate_a/t?client=dict-chrome-ex&sl=auto&tl=' . $langTarget . '&q=' . rawurlencode($originText)), true);
-                $targetData[$originText] = $arrayjson[0][0];
             }
-
+            file_put_contents($folderTarget . $langTarget . '-2.json', json_encode($targetData, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+            echo $folderTarget . ' Done...';
+            $targetData = [];
         }
-        file_put_contents($folderTarget . $langTarget . '-2.json', json_encode($targetData, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
-        echo $folderTarget . ' Done...';
-        $targetData = [];
     }
 }
